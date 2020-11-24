18ba4d78edada2c96e73d412e7e9ae1a2c8b91f8
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-11-24 19:49:53 -0300

    SystemUI: Implement Smart space from redfin 11
    Includes weather on lockscreen and cool stuff on ambient display
    
    Code reverse engineered from redfin RD1A.201105.003.C1
    
    Requires Google app installed
    
    Change-Id: Ice5ea4fd43412cf916daefa6521fc8b55f14f199

--- packages/SystemUI/src/com/google/android/systemui/statusbar/phone/dagger/StatusBarGoogleModule.java
+++ packages/SystemUI/src/com/google/android/systemui/statusbar/phone/dagger/StatusBarGoogleModule.java
@@ -104,6 +104,7 @@ import com.android.systemui.statusbar.phone.StatusBar;
 
 import com.google.android.systemui.LiveWallpaperScrimController;
 import com.google.android.systemui.statusbar.phone.StatusBarGoogle;
+import com.google.android.systemui.smartspace.SmartSpaceController;
 
 import java.util.Optional;
 import java.util.concurrent.Executor;
@@ -121,6 +122,7 @@ public interface StatusBarGoogleModule {
     @Provides
     @Singleton
     static StatusBar provideStatusBar(
+            SmartSpaceController smartSpaceController,
             Context context,
             NotificationsController notificationsController,
             LightBarController lightBarController,
@@ -202,6 +204,7 @@ public interface StatusBarGoogleModule {
             TunerService tunerService,
             FODCircleViewImpl fodCircleViewImpl) {
         return new StatusBarGoogle(
+                smartSpaceController,
                 context,
                 notificationsController,
                 lightBarController,
